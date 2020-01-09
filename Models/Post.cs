ee89e5ad93303e3717c5d7729cd9eb839299c8f2
ferizoozoo <farhadzohoorz@gmail.com>
2020-01-09 12:44:16 +0330

    Create the Post model

---
+++ Models/Post.cs
@@ -0,0 +1,13 @@
+
+namespace MiniBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public int DisLikes { get; set; }
+    }
+}
