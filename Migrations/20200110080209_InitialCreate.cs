07a420121fdf9ce154cbb2627ea6418dd678a799
ferizoozoo <farhadzohoorz@gmail.com>
2020-01-10 11:33:31 +0330

    Create the schema for Post and the database

---
+++ Migrations/20200110080209_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniBlog.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Author = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    Likes = table.Column<int>(nullable: false),
+                    DisLikes = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
