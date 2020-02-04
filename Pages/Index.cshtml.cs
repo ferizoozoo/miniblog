4f7999d6cf34ec6c179199cfcc7cb8c2bd18c08c
ferizoozoo <farhadzohoorz@gmail.com>
2020-02-04 10:51:31 +0330

    Add pagination

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -13,7 +14,8 @@ namespace MiniBlog.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        public MiniBlogContext _context { get; }
+        private readonly MiniBlogContext _context;
+        int pageSize = 3;
 
         public IndexModel(ILogger<IndexModel> logger, MiniBlogContext context)
         {
@@ -21,13 +23,16 @@ namespace MiniBlog.Pages
             _context = context;
         }
 
-        public IEnumerable<Post> Posts { get; private set; }
+        public PaginatedList<Post> Posts { get; private set; }
 
-        public void OnGet()
+        public async Task OnGetAsync(int? pageIndex)
         {
-            Posts = from post in _context.Posts
-                    orderby post descending
-                    select post;
+            IQueryable<Post> AllPosts = from s in _context.Posts
+                                        orderby s.Id descending
+                                        select s;
+
+            Posts = await PaginatedList<Post>.CreateAsync(
+                AllPosts.AsNoTracking(), pageIndex ?? 1, pageSize);        
         }
 
     }
